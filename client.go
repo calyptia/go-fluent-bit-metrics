202de4a9457444588eaf54d7e9a4d4dd8ca2ad19
Jorge Niedbalski <jnr@metaklass.org>
2021-08-04 10:18:31 -0400

    client: add storage metrics
    * add a call to api/v1/storage
    * adds a retry mechanism as storage metrics can take up to 3 secs to be ready.
    * uses a configuration file instead of cmd line parameters for tests.
    
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- client.go
+++ client.go
@@ -5,6 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+)
+
+const (
+	DefaultHTTPRetryTimeout = 3 * time.Second
+	DefaultHTTPRetryBackoff = 150 * time.Millisecond
 )
 
 // Client for Fluent Bit Monitoring HTTP API.
@@ -49,6 +55,36 @@ type MetricOutput struct {
 	RetriesFailed uint64 `json:"retries_failed"`
 }
 
+type PluginStorage struct {
+	Status struct {
+		Overlimit bool   `json:"overlimit"`
+		MemSize   string `json:"mem_size"`
+		MemLimit  string `json:"mem_limit"`
+	} `json:"status"`
+
+	Chunks struct {
+		Total    uint64 `json:"total"`
+		Up       uint64 `json:"up"`
+		Down     uint64 `json:"down"`
+		Busy     uint64 `json:"busy"`
+		BusySize string `json:"busy_size"`
+	} `json:"chunks"`
+}
+
+type StorageMetrics struct {
+	StorageLayer struct {
+		Chunks struct {
+			TotalChunks  uint64 `json:"total_chunks"`
+			MemChunks    uint64 `json:"mem_chunks"`
+			FsChunks     uint64 `json:"fs_chunks"`
+			FsChunksUp   uint64 `json:"fs_chunks_up"`
+			FsChunksDown uint64 `json:"fs_chunks_down"`
+		} `json:"chunks"`
+	} `json:"storage_layer"`
+
+	InputChunks map[string]PluginStorage `json:"input_chunks"`
+}
+
 func (c *Client) BuildInfo(ctx context.Context) (BuildInfo, error) {
 	var info BuildInfo
 	return info, c.fetchJSON(ctx, "/", &info)
@@ -64,15 +100,32 @@ func (c *Client) Metrics(ctx context.Context) (Metrics, error) {
 	return mm, c.fetchJSON(ctx, "/api/v1/metrics", &mm)
 }
 
+func (c *Client) StorageMetrics(ctx context.Context) (StorageMetrics, error) {
+	var mm StorageMetrics
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, DefaultHTTPRetryTimeout)
+	defer cancel()
+	return mm, c.fetchJSON(ctxWithTimeout, "/api/v1/storage", &mm)
+}
+
 func (c *Client) fetchJSON(ctx context.Context, endpoint string, ptr interface{}) error {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.BaseURL+endpoint, nil)
 	if err != nil {
 		return fmt.Errorf("could not create request: %w", err)
 	}
+	var resp *http.Response
+	ticker := time.NewTicker(DefaultHTTPRetryBackoff)
 
-	resp, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("could not do request: %w", err)
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("timeout while trying to reach: %s", endpoint)
+		case <-ticker.C:
+			resp, err = c.HTTPClient.Do(req)
+			if err == nil && resp.StatusCode != http.StatusNotFound {
+				break loop
+			}
+		}
 	}
 
 	defer resp.Body.Close()
@@ -80,7 +133,6 @@ func (c *Client) fetchJSON(ctx context.Context, endpoint string, ptr interface{}
 	if resp.StatusCode >= http.StatusBadRequest {
 		return fmt.Errorf("failed with status code %d", resp.StatusCode)
 	}
-
 	err = json.NewDecoder(resp.Body).Decode(ptr)
 	if err != nil {
 		return fmt.Errorf("could not json unmarshal response: %w", err)
